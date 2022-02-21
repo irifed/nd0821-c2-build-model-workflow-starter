536bce53704ea6b97bd7094e9ddb39c5520a0c1a
Irina Fedulova <fedulova@google.com>
2022-02-21 09:48:27 +0300

    found best hyperparams and implemented test_random_forest step

--- src/train_random_forest/run.py
+++ src/train_random_forest/run.py
@@ -28,7 +28,16 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
 from sklearn.pipeline import Pipeline, make_pipeline
 
-from feature_engineering import delta_date_feature
+#from feature_engineering import delta_date_feature
+def delta_date_feature(dates):
+    """
+    Given a 2d array containing dates (in any format recognized by pd.to_datetime), it returns the delta in days
+    between each date and the most recent date in its column
+    """
+    date_sanitized = pd.DataFrame(dates).apply(pd.to_datetime)
+    return date_sanitized.apply(lambda d: (d.max() -d).dt.days, axis=0).to_numpy()
+
+
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
