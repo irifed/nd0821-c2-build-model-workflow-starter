ab6d8923531dc428d35cf3cb9f649efa0f8ba76a
Irina Fedulova <fedulova@google.com>
2022-02-22 09:31:57 +0300

    updated basic cleaning: added docstrings

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -1,18 +1,32 @@
 #!/usr/bin/env python
 """
-Download from W&B the raw dataset and apply some basic data cleaning, exporting the result to a new artifact
+Download from W&B the raw dataset and apply some basic data cleaning,
+exporting the result to a new artifact
+
+Command line parameters:
+
+input_artifact: name of the input artifact (dataset)
+output_artifact: name of the output artifact (dataset)
+output_type: type of the output artifact
+output_description: description of the output artifact
+min_price: parameter for cleaning (removing price outliers) - min price
+max_price: parameter for cleaning (removing price outliers) - max price
+
+Run (from the root folder):
+mlflow run . -P steps=basic_cleaning
 """
 import argparse
 import logging
-import wandb
+
 import pandas as pd
+
+import wandb
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
 
 
 def go(args):
-
     run = wandb.init(job_type="basic_cleaning")
     run.config.update(args)
 
@@ -20,9 +34,6 @@ def go(args):
     # particular version of the artifact
     # artifact_local_path = run.use_artifact(args.input_artifact).file()
 
-    ######################
-    # YOUR CODE HERE     #
-    ######################
     logger.info("Downloading and reading test artifact")
     local_path = wandb.use_artifact(args.input_artifact).file()
     df = pd.read_csv(local_path)
@@ -51,46 +62,45 @@ def go(args):
 
 
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser(description="A very basic data cleaning")
 
     parser.add_argument(
-        "--input_artifact", 
+        "--input_artifact",
         type=str,
         help="Input artifact",
         required=True
     )
 
     parser.add_argument(
-        "--output_artifact", 
+        "--output_artifact",
         type=str,
         help="Output artifact",
         required=True
     )
 
     parser.add_argument(
-        "--output_type", 
+        "--output_type",
         type=str,
         help="output artifact type",
         required=True
     )
 
     parser.add_argument(
-        "--output_description", 
+        "--output_description",
         type=str,
         help="output artifact description",
         required=True
     )
 
     parser.add_argument(
-        "--min_price", 
+        "--min_price",
         type=float,
         help="min price",
         required=True
     )
 
     parser.add_argument(
-        "--max_price", 
+        "--max_price",
         type=float,
         help="max price",
         required=True
